6422c4ca7f0f1fd37631a9e5a5c679b6960356f5
Ken Gu <kgu@Kens-MacBook-Pro.local>
2020-09-09 17:01:46 -0700

    Finished

--- Lab1/Towers.java
+++ Lab1/Towers.java
@@ -3,19 +3,32 @@
 * Lab 1
 * java version "1.8.0_241", MacOS 10.15.6
 */
+
+/**
+* main()
+* @param args Commandline argument.
+* @return void
+* Used to pass parameters to towerOfHanoi().
+*/
 public class Towers{
   public static void main(String[] args) {
     towerOfHanoi(6, 'A', 'C', 'B');
   }
-
+  /**
+  * Method to display necessary moves to complete the Towers of Hanoi game.
+  * @param disk An integer; amount of disks.
+  * @param source Name of the starting tower.
+  * @param target Name of goal tower.
+  * @param spare Name of the rmaining tower.
+  */
   public static void towerOfHanoi(int disk, char source, char target, char spare){
     if(disk == 1){
-      System.out.println("Move top disk from "+source+" to "+target);
+      System.out.println("Move top disk from "+source+" to "+target); 
     }
     else{
-      towerOfHanoi(disk - 1, source, spare, target);
-      towerOfHanoi(1, source, target, spare);
-      towerOfHanoi(disk - 1, spare, target, source);
+      towerOfHanoi(disk - 1, source, spare, target); // All disks on spare when finished
+      towerOfHanoi(1, source, target, spare); // Bottom disk to target
+      towerOfHanoi(disk - 1, spare, target, source); // All disks from spare to target when finished.
     }
   }
 }
--- Lab1/TripleCallCounter.java
+++ Lab1/TripleCallCounter.java
@@ -3,3 +3,38 @@
 * Lab 1
 * java version "1.8.0_241", MacOS 10.15.6
 */
+
+/**
+* main()
+* @param args Command line argument.
+* @return void
+* Used to pass a hardcoded parameters to solveTripleCounter().
+*/
+class TripleCallCounter{
+  public static void main(String[] args) {
+    System.out.println(solveTripleCounter(-2)); //Prints the value returned by solveTripleCounter()
+  }
+
+  /**
+  * Method computes the recursive sequence T(n) = T(n-1) + T(1) + T(n-1)
+  * @param num An integer
+  * @return An integer. Returns -1 if num < 0.
+  * The non-recursive definition is T(n) = (2^n)-1. This
+  * can be seen with a table comparing n and T(n).
+  */
+  public static int solveTripleCounter(int num){
+    if(num >= 0){
+      if(num == 0 || num == 1){
+        return num;
+      }
+      else{
+        return solveTripleCounter(num - 1)
+        + solveTripleCounter(1)
+        + solveTripleCounter(num - 1);
+      }
+    }
+    else{
+      return -1;
+    }
+  }
+}
